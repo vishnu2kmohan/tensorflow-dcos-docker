8b5297e2f760e65f979bbae1b7504d21ae1620e3
Vishnu Mohan <vishnu@mesosphere.com>
2018-05-24 15:18:57 -0400

    Parameter servers listen on port 2223 by default

--- mnist_replica.sh
+++ mnist_replica.sh
@@ -2,35 +2,35 @@
 
 python mnist_replica.py \
     --existing_servers \
-    --ps_hosts=parameter-server-0-node.devtensorflowinteractive.autoip.dcos.thisdcos.directory:2222,parameter-server-1-node.devtensorflowinteractive.autoip.dcos.thisdcos.directory:2222 \
+    --ps_hosts=parameter-server-0-node.devtensorflowinteractive.autoip.dcos.thisdcos.directory:2223,parameter-server-1-node.devtensorflowinteractive.autoip.dcos.thisdcos.directory:2223 \
     --worker_hosts=worker-0-node.devtensorflowinteractive.autoip.dcos.thisdcos.directory:2222,worker-1-node.devtensorflowinteractive.autoip.dcos.thisdcos.directory:2222,worker-2-node.devtensorflowinteractive.autoip.dcos.thisdcos.directory:2222 \
     --job_name=ps \
     --task_index=0 &
     
 python mnist_replica.py \
     --existing_servers \
-    --ps_hosts=parameter-server-0-node.devtensorflowinteractive.autoip.dcos.thisdcos.directory:2222,parameter-server-1-node.devtensorflowinteractive.autoip.dcos.thisdcos.directory:2222 \
+    --ps_hosts=parameter-server-0-node.devtensorflowinteractive.autoip.dcos.thisdcos.directory:2223,parameter-server-1-node.devtensorflowinteractive.autoip.dcos.thisdcos.directory:2223 \
     --worker_hosts=worker-0-node.devtensorflowinteractive.autoip.dcos.thisdcos.directory:2222,worker-1-node.devtensorflowinteractive.autoip.dcos.thisdcos.directory:2222,worker-2-node.devtensorflowinteractive.autoip.dcos.thisdcos.directory:2222 \
     --job_name=ps \
     --task_index=1 &
      
 python mnist_replica.py \
     --existing_servers \
-    --ps_hosts=parameter-server-0-node.devtensorflowinteractive.autoip.dcos.thisdcos.directory:2222,parameter-server-1-node.devtensorflowinteractive.autoip.dcos.thisdcos.directory:2222 \
+    --ps_hosts=parameter-server-0-node.devtensorflowinteractive.autoip.dcos.thisdcos.directory:2223,parameter-server-1-node.devtensorflowinteractive.autoip.dcos.thisdcos.directory:2223 \
     --worker_hosts=worker-0-node.devtensorflowinteractive.autoip.dcos.thisdcos.directory:2222,worker-1-node.devtensorflowinteractive.autoip.dcos.thisdcos.directory:2222,worker-2-node.devtensorflowinteractive.autoip.dcos.thisdcos.directory:2222 \
     --job_name=worker \
     --task_index=0 &
     
 python mnist_replica.py \
     --existing_servers \
-    --ps_hosts=parameter-server-0-node.devtensorflowinteractive.autoip.dcos.thisdcos.directory:2222,parameter-server-1-node.devtensorflowinteractive.autoip.dcos.thisdcos.directory:2222 \
+    --ps_hosts=parameter-server-0-node.devtensorflowinteractive.autoip.dcos.thisdcos.directory:2223,parameter-server-1-node.devtensorflowinteractive.autoip.dcos.thisdcos.directory:2223 \
     --worker_hosts=worker-0-node.devtensorflowinteractive.autoip.dcos.thisdcos.directory:2222,worker-1-node.devtensorflowinteractive.autoip.dcos.thisdcos.directory:2222,worker-2-node.devtensorflowinteractive.autoip.dcos.thisdcos.directory:2222 \
     --job_name=worker \
     --task_index=1 &
 
 python mnist_replica.py \
     --existing_servers \
-    --ps_hosts=parameter-server-0-node.devtensorflowinteractive.autoip.dcos.thisdcos.directory:2222,parameter-server-1-node.devtensorflowinteractive.autoip.dcos.thisdcos.directory:2222 \
+    --ps_hosts=parameter-server-0-node.devtensorflowinteractive.autoip.dcos.thisdcos.directory:2223,parameter-server-1-node.devtensorflowinteractive.autoip.dcos.thisdcos.directory:2223 \
     --worker_hosts=worker-0-node.devtensorflowinteractive.autoip.dcos.thisdcos.directory:2222,worker-1-node.devtensorflowinteractive.autoip.dcos.thisdcos.directory:2222,worker-2-node.devtensorflowinteractive.autoip.dcos.thisdcos.directory:2222 \
     --job_name=worker \
     --task_index=2 &
